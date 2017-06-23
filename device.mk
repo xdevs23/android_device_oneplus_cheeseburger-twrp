32f95c5470f27cf70b57c9e248b22e0e4a2db5c3
Simao Gomes Viana <xdevs23@outlook.com>
2017-06-23 18:43:36 +0200

    device.mk: include all files in root dir

--- device.mk
+++ device.mk
@@ -24,5 +24,5 @@ PRODUCT_AAPT_PREF_CONFIG := xxxhdpi
 # TWRP
 PRODUCT_COPY_FILES += \
 	$(PLATFORM_PATH)/recovery.fstab:recovery/root/etc/twrp.fstab \
-	$(foreach _,$(shell find $(PLATFORM_PATH)/recovery -name '*.so'),\
+	$(foreach _,$(shell find $(PLATFORM_PATH)/recovery/root -type f),\
 		$_:$(subst $(PLATFORM_PATH),$_,))
