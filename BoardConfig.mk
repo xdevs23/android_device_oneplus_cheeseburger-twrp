97ba67ff545891a9efa22ba774bf9ce9ed2dbc6f
Simao Gomes Viana <xdevs23@outlook.com>
2018-01-01 16:34:27 +0100

    Remove duplicate hwd enc

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -39,9 +39,6 @@ TARGET_2ND_CPU_ABI := armeabi-v7a
 TARGET_2ND_CPU_ABI2 := armeabi
 TARGET_2ND_CPU_VARIANT := cortex-a53
 
-# Crypto
-TARGET_HW_DISK_ENCRYPTION := true
-
 # Kernel
 BOARD_KERNEL_CMDLINE := \
         androidboot.hardware=qcom \
